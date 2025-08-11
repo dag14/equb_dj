88c31bfc293cf859b725efa2fe9b5c6cd7e246e9
Dagim Asrat <dagimasrat3@gmail.com>
2025-08-11 11:49:18 +0300

    master checkpoint

--- equb/views/equb_group_view.py
+++ equb/views/equb_group_view.py
@@ -5,7 +5,7 @@ from equb.serializers.equb_group_serializer import EqubGroupSerializer
 class EqubGroupViewSet(viewsets.ModelViewSet):
     queryset = EqubGroup.objects.all()
     serializer_class = EqubGroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
