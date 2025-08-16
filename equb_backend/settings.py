fe1c18f7666e344b5a05a7504d132e862c2016b3
Dagim Asrat <dagimasrat3@gmail.com>
2025-08-16 08:09:37 +0300

    minor changes on group member

--- equb_backend/settings.py
+++ equb_backend/settings.py
@@ -98,8 +98,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'equb_backend.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
